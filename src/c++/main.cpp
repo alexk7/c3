cf52049c7d846000b32a21f5741b9feeba95f7d5
Alexandre Cossette <alex.cossette@gmail.com>
2011-06-21 11:13:35 -0400

    added project file

---
+++ src/c++/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  c3
+//
+//  Created by Alexandre Cossette on 11-06-21.
+//  Copyright 2011 alexk7. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
